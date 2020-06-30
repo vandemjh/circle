e2974e76b3b7c3fa2b1fce899ea370a822072280
vandemjh <jack.vandem@gmail.com>
2020-06-30 16:28:23 -0400

    added comments under card posts

--- src/app/post/comments/comments.component.ts
+++ src/app/post/comments/comments.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Comment} from '../../classes/comment/comment'
 
 @Component({
   selector: 'circle-comments',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent implements OnInit {
-
+  @Input() comments: Comment[];
   constructor() { }
 
   ngOnInit(): void {
