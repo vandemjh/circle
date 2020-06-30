4ea7c6a534f8aaabe563fa781c5e1c4e633f3c6c
vandemjh <jack.vandem@gmail.com>
2020-06-30 14:45:59 -0400

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { NewPostComponent } from './new/new-post/new-post.component';
 import { NewPostButtonComponent } from './new/new-post-button/new-post-button.component';
 import { CommentsComponent } from './post/comments/comments.component';
 import { PostComponent } from './post/post/post.component';
 import { NewPostFormComponent } from './new/new-post-form/new-post-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NewPostComponent,
     NewPostButtonComponent,
     CommentsComponent,
     PostComponent,
@@ -22,7 +21,8 @@ import { NewPostFormComponent } from './new/new-post-form/new-post-form.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
