fbfa92f1994c3f0f710f70b7d6ee5a9062fae780
vandemjh <jack.vandem@gmail.com>
2020-07-07 15:49:01 -0400

    added httpmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -43,9 +44,10 @@ import { MatDialogModule } from "@angular/material/dialog";
         MatExpansionModule,
         MatInputModule,
         MatListModule,
-        MatDialogModule,
+        MatDialogModule, 
+        HttpClientModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
