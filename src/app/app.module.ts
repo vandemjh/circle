22ec39942a908680fc48de14935a710c6f61a496
vandemjh <jack.vandem@gmail.com>
2020-07-23 19:02:22 -0400

    added forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -46,6 +47,7 @@ import { MatDialogModule } from "@angular/material/dialog";
         MatListModule,
         MatDialogModule, 
         HttpClientModule,
+        ReactiveFormsModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
