b1591a2066b52e88a40c9520691104a6fa1e54b7
vandemjh <jack.vandem@gmail.com>
2020-07-25 15:34:26 -0400

    Additions to new post button

--- src/app/new/new-post-form/new-post-form.component.ts
+++ src/app/new/new-post-form/new-post-form.component.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { Post } from '../../classes/post/post'
-import { CircleService } from '../../services/circle.service'
+import { Post } from '../../classes/post/post';
+import { CircleService } from '../../services/circle.service';
 
 @Component({
-    selector: "circle-new-post-form",
-    templateUrl: "./new-post-form.component.html",
-    styleUrls: ["./new-post-form.component.css"],
+  selector: 'circle-new-post-form',
+  templateUrl: './new-post-form.component.html',
+  styleUrls: ['./new-post-form.component.css'],
 })
 export class NewPostFormComponent implements OnInit {
-    toPost: Post;
-    postForm = new FormGroup({
-        poster: new FormControl(''),
-        location: new FormControl(''),
-        imageUrl: new FormControl(''),
-        description: new FormControl(''),
-    });
-    constructor(private circleService: CircleService) { }
+  toPost: Post;
+  postForm = new FormGroup({
+    poster: new FormControl(''),
+    location: new FormControl(''),
+    imageUrl: new FormControl(''),
+    description: new FormControl(''),
+  });
+  constructor(private circleService: CircleService) {}
 
-    ngOnInit(): void {
-        this.circleService.getPosts().subscribe(x => console.log(x))
-     }
-    onSubmit() {
-        this.circleService.submitPost(new Post(
-            this.postForm.value.imageUrl,
-        )).subscribe(x => console.log(x));
-    }
+  ngOnInit(): void {
+    this.circleService.getPosts().subscribe((x) => console.log(x));
+  }
+  onSubmit() {
+    this.circleService
+      .submitPost(new Post(this.postForm.value.imageUrl))
+      .subscribe((x) => console.log(x));
+  }
 }
