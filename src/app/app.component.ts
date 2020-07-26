f16b2debd0bb12e988223ff153d65033d39d4956
vandemjh <jack.vandem@gmail.com>
2020-07-26 12:30:06 -0400

    added for loop for posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import {CircleService} from './services/circle.service'
+import {Post} from './classes/post/post';
 
 @Component({
     selector: "circle-root",
@@ -6,5 +8,23 @@ import { Component } from "@angular/core";
     styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-    title = "circle";
+    title = "Circle";
+    posts: Post[];
+    constructor(private service: CircleService) {}
+    
+    ngOnInit(): void {
+        this.posts = [];
+        // this.posts = [
+        //     new Post("asdf", "123", "brownsville", "this is description"),
+        //     new Post("asdf", "123", "brownsville", "this is description"),
+        //     new Post("asdf", "123", "brownsville", "this is description"),
+        // ]
+    }
+
+    getPosts(): void {
+        this.posts = [
+            new Post("asdf", "123", "brownsville", "this is description")
+        ]
+        // this.service.getPosts().subscribe();
+    }
 }
