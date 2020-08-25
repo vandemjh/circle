46fa0c6ea80e6dc58a6b531e7281857194b9ae04
vandemjh <jack.vandem@gmail.com>
2020-08-24 21:25:10 -0400

    improved rsync args

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,9 +1,9 @@
 # Raspberry pi is too slow to build using Docker...
 ng build --configuration=local
-rsync -rvi --delete --progress dist/circle/ pi@circle.local:/var/www/html/
+rsync -ri --delete dist/circle/ pi@circle.local:/var/www/html/
 
 # -v, --verbose               increase verbosity
-#  -r, --recursive             recurse into directories
-#  --delete                delete extraneous files from destination dirs
-#  --progress              show progress during transfer
-#  -i, --itemize-changes       output a change-summary for all updates
+# -r, --recursive             recurse into directories
+# --delete                delete extraneous files from destination dirs
+# --progress              show progress during transfer
+# -i, --itemize-changes       output a change-summary for all updates
